e2cd0002ca83da1f22c4df4aa86d0a3a31ef0629
Catarina Batista <catarinabatistayou@gmail.com>
2019-05-20 11:40:48 +0100

    tudo

---
+++ BookingProjeto/Booking/Models/ViewModel.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Booking.Models
+{
+    public class ViewModel
+
+    {
+        public ViewModel(List<QuartosDisp> quartos, List<String> tipo)
+        {
+            Quartos = quartos;
+            Tipo = tipo;
+
+        }
+
+        public List<String> Tipo { get; private set; }
+        public List<QuartosDisp> Quartos { get; private set; }
+    }
+}
